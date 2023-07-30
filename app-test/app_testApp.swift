aadf5fbf88f390b2f9d99d140cc5a450615e7269
gssp <gssp@gssps-MacBook-Pro.local>
2023-07-29 17:51:51 -0700

    init commit

---
+++ app-test/app_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  app_testApp.swift
+//  app-test
+//
+//  Created by gssp on 7/29/23.
+//
+
+import SwiftUI
+
+@main
+struct app_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
